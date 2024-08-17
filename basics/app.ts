06a83200c3c19c3641621ccc115e1eb7cf5bee3d
hardik200404 <Polenati.hardik@gmail.com>
2024-08-17 19:21:55 +0530

    Arrays and Objects

--- basics/app.ts
+++ basics/app.ts
@@ -2,12 +2,22 @@ const num1Elem = document.getElementById('num1') as HTMLInputElement
 const num2Elem = document.getElementById('num2') as HTMLInputElement
 const Add_Btn = document.querySelector('button')! 
 
-function addNums(num1:number,num2:number){
-    return num1 + num2;
+const nums: number[] = []
+
+function addNums(num1: number, num2: number){
+    return num1 + num2
+}
+
+function print_obj(obj: { val: number; time: Date}){
+    console.log(obj)
 }
 
 Add_Btn.addEventListener('click',() => {
     const num1 = num1Elem.value
     const num2 = num2Elem.value
-    console.log(addNums(+num1, +num2))
+    const result = addNums(+num1, +num2)
+    console.log(result)
+    print_obj({ val: result , time: new Date()})
+    nums.push(result as number)
+    console.log(nums)
 }) 
